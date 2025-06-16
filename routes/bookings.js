acd05228d361971db1080a66b49089dfdb693b20
rraushan378 <55614007+rraushan378@users.noreply.github.com>
2025-06-16 17:01:18 +0530

    Create bookings.js

---
+++ routes/bookings.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const bookingsController = require('../controllers/bookingsController');
+
+router.post('/', bookingsController.createBooking);
+router.get('/:id', bookingsController.getBookingById);
+
+module.exports = router;
